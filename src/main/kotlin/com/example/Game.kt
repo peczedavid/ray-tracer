fbde673bea0322eabf6e488c6be5aabc34082a2b
peczedavid <peczedavid@gmail.com>
2022-05-28 12:13:52 +0200

    Render scaling :camera:

--- src/main/kotlin/com/example/Game.kt
+++ src/main/kotlin/com/example/Game.kt
@@ -35,12 +35,16 @@ class Game : Application() {
     private var cameraFov = 60f
 
     private val ambientColor = Vector3(0.25f, 0.25f, 0.25f)
-    private var renderScale = 6
+    private val movingScale = 10
+    private var renderScaleSetting = 3
+    private var renderScale = renderScaleSetting
+    private var hud = true
 
     private lateinit var skySphere : Sphere
     private lateinit var pixelWriter: PixelWriter
 
     // use a set so duplicates are not possible
+    private var keysBefore = mutableSetOf<KeyCode>()
     private val currentlyActiveKeys = mutableSetOf<KeyCode>()
 
     private lateinit var canvas : Canvas
@@ -71,19 +75,25 @@ class Game : Application() {
 
         val uvImage = Image("/uv-test.png")
 
+        val brass =
+            ReflectiveMaterial(Vector3(0.444f, 0.527f, 1.094f), Vector3(3.695f, 2.765f, 1.829f))
+        val copper =
+            ReflectiveMaterial(Vector3(0.27105f, 0.67693f, 1.3164f), Vector3(3.6092f, 2.6248f, 2.2921f))
+        val mercury =
+            ReflectiveMaterial(Vector3(2.0733f, 1.55230f, 1.0606f), Vector3(5.3383f, 4.651f, 3.8628f))
+        val iron =
+            ReflectiveMaterial(Vector3(2.9114f, 2.9497f, 2.5845f), Vector3(3.0893f, 2.9318f, 2.767f))
+        val silver =
+            ReflectiveMaterial(Vector3(0.15943f, 0.14512f, 0.13547f), Vector3(3.9291f, 3.19f, 2.3808f))
+        val gold =
+            ReflectiveMaterial(Vector3(0.18299f, 0.42108f, 1.37340f), Vector3(3.4242f, 2.3459f, 1.7704f))
         val groundTexture =
             RoughMaterial(Image("/checker-board.jpg"), Vector3(0f, 0f, 0f))
-        val reflectiveMaterial =
-            ReflectiveMaterial(Vector3(1f, 1f, 1f), Vector3(5f, 5f, 5f))
-        val blueReflectiveMaterial =
+        val glass =
+            ReflectiveMaterial(Vector3(1.5f, 1.5f, 1.5f), Vector3(5f, 5f, 5f))
+        val blueGlassMaterial =
             ReflectiveMaterial(Vector3(1f, 1f, 1f), Vector3(3f, 3f, 10f))
-        val blueMaterial =
-            RoughMaterial(Vector3(0.2f, 0.4f, 0.8f), Vector3(0.2f, 0.4f, 0.8f), 100f)
-        val greenMaterial =
-            RoughMaterial(Vector3(0.2f, 0.8f, 0.4f), Vector3(0.2f, 0.8f, 0.4f), 100f)
-        val redMaterial =
-            RoughMaterial(Vector3(0.8f, 0.2f, 0.2f), Vector3(0.8f, 0.2f, 0.2f), 100f)
-        val imageMaterial =
+        val uvMaterial =
             RoughMaterial(uvImage, Vector3(0f, 0f, 0f), 100f)
         val skyMaterial =
             RoughMaterial(Image("/HDR_029_Sky_Cloudy_Bg.jpg"), Vector3(0f, 0f, 0f))
@@ -92,9 +102,9 @@ class Game : Application() {
         skySphere = Sphere(Vector3(0f, 0f, 0f), 100f, skyMaterial)
         sceneObjects.add(skySphere)
 
-        sceneObjects.add(Sphere(Vector3(2f, 1f, 0f), 1f, blueReflectiveMaterial))
-        sceneObjects.add(Sphere(Vector3(0f, 1f, -2f), 1f, imageMaterial))
-        sceneObjects.add(Sphere(Vector3(-2f, 1f, 0f), 1f, reflectiveMaterial))
+        sceneObjects.add(Sphere(Vector3(2f, 1f, 0f), 1f, brass))
+        sceneObjects.add(Sphere(Vector3(0f, 1f, -2f), 1f, copper))
+        sceneObjects.add(Sphere(Vector3(-2f, 1f, 0f), 1f, silver))
         sceneObjects.add(Plane(Vector3(0f, 0f, 0f), Vector3(0f, 1f, 0f), groundTexture).apply { scale = 0.2f })
 
         // Main loop
@@ -134,7 +144,7 @@ class Game : Application() {
     }
 
     private fun trace(ray: Ray, depth : Int = 0) : Vector3 {
-        if(depth > 2) return ambientColor
+        if(depth > 3) return ambientColor
         val hit = firstHit(ray)
         if(hit.t < 0f) return ambientColor
 
@@ -173,54 +183,71 @@ class Game : Application() {
         return outColor
     }
 
-    private fun controlCamera(dt: Float) {
+    private fun controlCamera(dt: Float) : Boolean {
         val speed = 4f
         val facing = normalize(camera.lookAt - camera.position)
         val fov = cameraFov * PI.toFloat() / 180f
         val vup = Vector3(0f, 1f, 0f)
+        var moved = false
         if(currentlyActiveKeys.contains(KeyCode.W)) {
             val delta = facing * speed * dt
             val newPos = camera.position + delta
             val newLookAt = camera.lookAt + delta
             camera.set(newPos, newLookAt, vup, fov)
+            moved = true
         }
         if(currentlyActiveKeys.contains(KeyCode.S)) {
             val delta = facing * speed * dt
             val newPos = camera.position - delta
             val newLookAt = camera.lookAt - delta
             camera.set(newPos, newLookAt, vup, fov)
+            moved = true
         }
         if(currentlyActiveKeys.contains(KeyCode.D)) {
             val delta = camera.right * speed * dt
             val newPos = camera.position + delta
             val newLookAt = camera.lookAt + delta
             camera.set(newPos, newLookAt, vup, fov)
+            moved = true
         }
         if(currentlyActiveKeys.contains(KeyCode.A)) {
             val delta = camera.right * speed * dt
             val newPos = camera.position - delta
             val newLookAt = camera.lookAt - delta
             camera.set(newPos, newLookAt, vup, fov)
+            moved = true
         }
         if(currentlyActiveKeys.contains(KeyCode.SPACE)) {
             val delta = Vector3(0f, 1f, 0f) * speed * dt
             val newPos = camera.position + delta
             val newLookAt = camera.lookAt + delta
             camera.set(newPos, newLookAt, vup, fov)
+            moved = true
         }
         if(currentlyActiveKeys.contains(KeyCode.C)) {
             val delta = Vector3(0f, 1f, 0f) * speed * dt
             val newPos = camera.position - delta
             val newLookAt = camera.lookAt - delta
             camera.set(newPos, newLookAt, vup, fov)
+            moved = true
         }
+        return moved
     }
 
     private fun controlScale() {
-        if(currentlyActiveKeys.contains(KeyCode.E)) renderScale -= 1
-        if(currentlyActiveKeys.contains(KeyCode.Q)) renderScale += 1
+        if(currentlyActiveKeys.contains(KeyCode.E)) renderScaleSetting -= 1
+        if(currentlyActiveKeys.contains(KeyCode.Q)) renderScaleSetting += 1
 
-        renderScale = renderScale.coerceIn(1, 30)
+        renderScaleSetting = renderScaleSetting.coerceIn(1, 30)
+    }
+
+    private fun keyJustPressed(keyCode: KeyCode) : Boolean {
+        return !keysBefore.contains(keyCode) && currentlyActiveKeys.contains(keyCode)
+    }
+
+    private fun controlHud() {
+        if(keyJustPressed(KeyCode.H))
+            hud = !hud
     }
 
     private fun tickAndRender(currentNanoTime: Long) {
@@ -236,8 +263,10 @@ class Game : Application() {
 
         // clear canvas
         graphicsContext.clearRect(0.0, 0.0, WIDTH.toDouble(), HEIGHT.toDouble())
-        controlCamera(elapsedMs / 1000f)
+        val moved = controlCamera(elapsedMs / 1000f)
+        renderScale = if(moved) movingScale else renderScaleSetting
         controlScale()
+        controlHud()
 
         for(y in 0 until HEIGHT step renderScale) {
             for(x in 0 until WIDTH step renderScale) {
@@ -258,10 +287,13 @@ class Game : Application() {
         // display crude fps counter
 
         if (elapsedMs != 0L) {
-            graphicsContext.fill = Color.WHITE
-            graphicsContext.font = Font.font(22.0)
-            //graphicsContext.fillText("${1000 / elapsedMs} fps - $renderScale:1", 10.0, 27.0)
-            //graphicsContext.fillText("$WIDTH x $HEIGHT", 10.0, (HEIGHT - 10).toDouble())
+            if(hud) {
+                graphicsContext.fill = Color.WHITE
+                graphicsContext.font = Font.font(22.0)
+                graphicsContext.fillText("${1000 / elapsedMs} fps - $renderScale:1", 10.0, 27.0)
+                graphicsContext.fillText("$WIDTH x $HEIGHT", 10.0, (HEIGHT - 10).toDouble())
+            }
         }
+        keysBefore = currentlyActiveKeys.toMutableSet()
     }
 }
